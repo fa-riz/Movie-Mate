4efc7705ea4184af4b32171a73057d49a8bd633e
Muhammed Fariz P A <muhammedfarizpa@gmail.com>
2025-10-17 21:12:14 +0530

    docker added

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
 // src/utils/constants.js
-export const API_BASE = "http://localhost:8000";
+export const API_BASE = "https://movie-mate-2.onrender.com";
 export const TMDB_IMAGE_BASE = "https://image.tmdb.org/t/p/w500";
